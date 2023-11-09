a8f319a7113cd2ae6e865072298644837f780540
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-09 13:14:14 +0300

    Добавьте файлы проекта.

---
+++ Skymey-crypto-binance-current-price/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_crypto_binance_current_price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
